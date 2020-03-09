691068fb120b0256223bf93ae819697b6036c0dd
misterplus <whiteplayz45@gmail.com>
2020-03-09 12:52:06 +0800

    Version 1.3.5

--- src/main/java/com/misterplus/plustweaks/compact/crafttweaker/LiquidProperties.java
+++ src/main/java/com/misterplus/plustweaks/compact/crafttweaker/LiquidProperties.java
@@ -8,14 +8,9 @@ import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
-import java.util.LinkedList;
-import java.util.List;
-
 @ZenRegister
 @ZenClass("mods.plustweaks.LiquidProperties")
 public class LiquidProperties {
-
-    public static List<LiquidProperties> ctInfinites = new LinkedList<>();
 
     @ZenMethod
     public static void setInfinite(ILiquidStack liquidStack, @Optional boolean finite) {
