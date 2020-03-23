5625ccf827084b9f6a8cc830aef9f6d13e15e804
misterplus <whiteplayz45@gmail.com>
2020-03-23 20:29:19 +0800

    Version 1.3.7

--- src/main/java/plus/misterplus/plustweaks/PlusTweaks.java
+++ src/main/java/plus/misterplus/plustweaks/PlusTweaks.java
@@ -22,7 +22,7 @@ import static plus.misterplus.plustweaks.config.Configs.genericSettings;
 public class PlusTweaks {
     public static final String MOD_ID = "plustweaks";
     public static final String MOD_NAME = "PlusTweaks";
-    public static final String VERSION = "1.3.6";
+    public static final String VERSION = "1.3.7";
 
     public static Logger logger = LogManager.getLogger(PlusTweaks.MOD_NAME);
 
